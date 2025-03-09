b219ed978d5963682c32b72ab2214f27a4840c59
Abdul Rafay <99marafay@gmail.com>
2025-03-10 02:47:20 +0500

    API key removes

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,10 +1,10 @@
 class FirebaseConfig {
-  static const String apiKey = 'AIzaSyCs14O9rfNaFgPBlN1Z3_2coBpLdY_UI9w';
-  static const String appId = '1:689509093484:android:56039ff98006d7766ac849';
-  static const String messagingSenderId = '689509093484';
-  static const String projectId = 'msbridge-9a2c7';
-  static const String storageBucket = 'msbridge-9a2c7.firebasestorage.app';
-  static const String iosBundleId = 'com.syntaxlab.msbridge';
+  static const String apiKey = '';
+  static const String appId = '';
+  static const String messagingSenderId = '';
+  static const String projectId = '';
+  static const String storageBucket = '';
+  static const String iosBundleId = '';
 }
 
 class APIConfig {
