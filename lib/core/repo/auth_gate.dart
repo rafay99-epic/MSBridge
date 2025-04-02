d8a3fe88873a749531b377bda5728917ec1df3df
Abdul Rafay <99marafay@gmail.com>
2025-04-02 05:00:21 +0500

    fingerprint lock

--- lib/core/repo/auth_gate.dart
+++ lib/core/repo/auth_gate.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:msbridge/core/repo/auth_repo.dart';
 import 'package:msbridge/features/auth/verify/verify_email.dart';
 import 'package:msbridge/features/home/home.dart';
+import 'package:msbridge/features/lock/fingerprint_lock_screen.dart';
 import 'package:msbridge/features/splash/splash_screen.dart';
 
 class AuthGate extends StatelessWidget {
@@ -19,13 +20,17 @@ class AuthGate extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.active) {
             User? user = snapshot.data;
 
+            Widget child;
+
             if (user == null) {
-              return const SplashScreen();
+              child = const SplashScreen();
             } else if (!user.emailVerified) {
-              return const EmailVerificationScreen();
+              child = const EmailVerificationScreen();
             } else {
-              return const Home();
+              child = const Home();
             }
+
+            return FingerprintAuthWrapper(child: child);
           }
 
           return const Center(child: CircularProgressIndicator());
