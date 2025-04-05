7a64588f39335964ef4b9a96e39d0f96a2d882ed
Abdul Rafay <99marafay@gmail.com>
2025-04-05 18:40:26 +0500

    Bug fix, Building Singed apk File

--- android/app/src/main/kotlin/com/syntaxlab/msbridge/MainActivity.kt
+++ android/app/src/main/kotlin/com/syntaxlab/msbridge/MainActivity.kt
@@ -1,9 +1,14 @@
-// Orginal Code without Fingerprint lock
 package com.syntaxlab.msbridge
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
+
+// Enable this if you want to use fingperprint authentication in your app
+// but there is a bug  that is if you enable this and if you use system back button then it will close the application 
+// and  not open the previous screen.
+// If you want to use this then uncomment the below code and comment the above code
 
 
 // package com.syntaxlab.msbridge
